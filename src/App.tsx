9a5b0d42526ff93b1e1eb3e9005f55c65479172d
guesung <gueit214@naver.com>
2025-09-05 14:17:05 +0900

    chore: 홈 페이지 필요한 경우에만 불러옴

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
-import Home from './pages/Home/Home';
-
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
 
 import './App.css';
 
 // Search 페이지를 동적으로 로드하여 Home 페이지 번들에서 분리
+const Home = lazy(() => import('./pages/Home/Home'));
 const Search = lazy(() => import('./pages/Search/Search'));
 
 const App = () => {
@@ -17,13 +16,13 @@ const App = () => {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route 
-          path="/search" 
+        <Route
+          path="/search"
           element={
             <Suspense fallback={<div>Loading...</div>}>
               <Search />
             </Suspense>
-          } 
+          }
         />
       </Routes>
       <Footer />
