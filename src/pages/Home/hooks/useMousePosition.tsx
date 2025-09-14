64ad0542789831e2bbf962ad3e0bb61a36a0b110
guesung <gueit214@naver.com>
2025-09-14 16:53:36 +0900

    chore: rAF로 커서 애니메이션 최적화

--- src/pages/Home/hooks/useMousePosition.tsx
+++ src/pages/Home/hooks/useMousePosition.tsx
@@ -1,39 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 
-export type MousePosition = Partial<MouseEvent>;
-
-const useMousePosition = () => {
-  const [mousePosition, setMousePosition] = useState<MousePosition>({
-    clientX: 0,
-    clientY: 0,
-    pageX: 0,
-    pageY: 0,
-    offsetX: 0,
-    offsetY: 0
-  });
-
-  const updateMousePosition = (e: MouseEvent) => {
-    const { clientX, clientY, pageX, pageY, offsetX, offsetY } = e;
-
-    setMousePosition({
-      clientX,
-      clientY,
-      pageX,
-      pageY,
-      offsetX,
-      offsetY
-    });
-  };
+const useMousePosition = (cursorRef: React.RefObject<HTMLDivElement>) => {
+  const ticking = useRef(false);
 
   useEffect(() => {
-    window.addEventListener('mousemove', updateMousePosition);
+    const updateCursorPosition = (x: number, y: number) => {
+      if (cursorRef.current) {
+        // transform 속성으로 GPU 가속 활용
+        cursorRef.current.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+      }
 
-    return () => {
-      window.removeEventListener('mousemove', updateMousePosition);
+      // 애니메이션 프레임이 끝났음을 표시
+      ticking.current = false;
     };
-  }, []);
 
-  return mousePosition;
+    let rafID: number | null = null;
+    const handleMouseMove = (e: MouseEvent) => {
+      const { pageX, pageY } = e;
+      if (!ticking.current) {
+        // rAF를 활용하여 브라우저 리페인트 주기에 동기화
+        // 새로운 애니메이션 프레임 요청, 애니메이션 예약 - ticking 플래그 설정
+        rafID = requestAnimationFrame(() => updateCursorPosition(pageX, pageY));
+        ticking.current = true;
+      }
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+    return () => {
+      // 이벤트 리스너 제거, 애니메이션 프레임 취소
+      window.removeEventListener('mousemove', handleMouseMove);
+      if (rafID !== null) {
+        cancelAnimationFrame(rafID);
+      }
+    };
+  }, [cursorRef]);
 };
-
 export default useMousePosition;
