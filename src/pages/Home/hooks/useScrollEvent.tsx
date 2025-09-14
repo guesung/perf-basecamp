67319e8bc3b55c3d0824c0748518aa514459a59a
guesung <gueit214@naver.com>
2025-09-14 16:58:06 +0900

    chore: rAF로 스크롤 애니메이션 최적화

--- src/pages/Home/hooks/useScrollEvent.tsx
+++ src/pages/Home/hooks/useScrollEvent.tsx
@@ -1,17 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 
 type ScrollHandler = () => void;
 
 const useScrollEvent = (onScroll: ScrollHandler) => {
+  const ticking = useRef(false);
+
   useEffect(() => {
-    const handleScroll = (event: Event) => {
-      onScroll();
+    let rafID: number | null = null;
+    const handleScroll = () => {
+      if (!ticking.current) {
+        rafID = requestAnimationFrame(() => {
+          onScroll();
+        });
+        ticking.current = true;
+      }
     };
 
     window.addEventListener('scroll', handleScroll);
 
     return () => {
       window.removeEventListener('scroll', handleScroll);
+      if (rafID !== null) {
+        cancelAnimationFrame(rafID);
+      }
     };
   }, [onScroll]);
 };
